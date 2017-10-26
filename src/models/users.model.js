e68b23ebdf9a5fbb6e8b0ec4085a96aeff3f9126
Jan-Sijmen <js.zwarts@gmail.com>
2017-10-26 13:59:24 +0200

    Adds users model and authentication

---
+++ src/models/users.model.js
@@ -0,0 +1,21 @@
+// users-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+
+  const users = new mongooseClient.Schema({
+
+    email: { type: String, unique: true },
+    password: { type: String },
+
+    gymId: { type: Schema.Types.ObjectId, ref: 'gyms' },
+
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
+
+  return mongooseClient.model('users', users);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const gyms = require('./gyms/gyms.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(gyms);
+  app.configure(users);
 };
